555778b76fb2281f9802205e47b9a9cf2b1ce30d
Killian ALLAIRE <killian.allaire@hotmail.com>
2023-11-13 22:49:39 +0100

    feat: switched everything from gin to http/net

--- api/v1/handlers/experience.go
+++ api/v1/handlers/experience.go
@@ -1,44 +1,64 @@
 package handlers
 
 import (
+	"encoding/json"
 	"golang-rest-api-portfolio/api/infra/storage"
 	"golang-rest-api-portfolio/api/v1/models"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
-func GetExperiences(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, storage.GetExperiences())
+func GetExperiences(w http.ResponseWriter, r *http.Request) {
+	res, err := json.Marshal(storage.GetExperiences())
+
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(res))
 }
 
-func PostExperience(c *gin.Context) {
+func PostExperience(w http.ResponseWriter, r *http.Request) {
 	var newExperience models.Experience
 
-	if err := c.BindJSON(&newExperience); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&newExperience); err != nil {
 		return
 	}
 
 	storage.AddExperience(newExperience)
-	c.IndentedJSON(http.StatusCreated, newExperience)
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("Post JSON")) // Experience
 }
 
-func GetExperienceByID(c *gin.Context) {
-	id := c.Param("id")
+func GetExperienceByID(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path
 
 	if experience := storage.GetExperienceByID(id); experience == nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "experience not found"})
+		w.WriteHeader(http.StatusBadRequest)
 	} else {
-		c.IndentedJSON(http.StatusOK, experience)
+		w.WriteHeader(http.StatusOK)
+		res, err := json.Marshal(experience)
+		if err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+		w.Write([]byte(res))
 	}
 }
 
-func DeleteExperience(c *gin.Context) {
-	id := c.Param("id")
+func DeleteExperience(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path
 
 	if deleted_id := storage.RemoveExperience(id); deleted_id == "" {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "experience id not found"})
+		w.WriteHeader(http.StatusBadRequest)
 	} else {
-		c.IndentedJSON(http.StatusOK, deleted_id)
+		w.WriteHeader(http.StatusOK)
+		res, err := json.Marshal(deleted_id)
+		if err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+		w.Write([]byte(res))
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"golang-rest-api-portfolio/api/v1/routers"
 )
 
+const Port = "8080"
+
 func main() {
 	routers.SetupPing("/api")
-	http.ListenAndServe(":8080", nil)
+	routers.SetupExperience("/api/v1")
+	fmt.Print("Starting http server on port " + Port)
+	err := http.ListenAndServe(":"+Port, nil)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
