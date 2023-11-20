855e799e05aa00169e023a955ed1cc9501160658
Killian ALLAIRE <killian.allaire@hotmail.com>
2023-11-20 23:00:32 +0100

    added simple cors rule. In the future, will need to make it more restrictive

--- api/v1/routers/project.go
+++ api/v1/routers/project.go
@@ -2,29 +2,33 @@ package routers
 
 import (
 	"golang-rest-api-portfolio/api/v1/handlers"
+	"golang-rest-api-portfolio/api/v1/middleware"
 	"net/http"
 )
 
 func SetupProject(prefix string) {
 	http.HandleFunc(prefix+"/projects",
-		func(w http.ResponseWriter, r *http.Request) {
-			switch r.Method {
-			case "GET":
-				handlers.GetProjects(w, r)
-			case "POST":
-				handlers.PostProject(w, r)
-			}
-
-		})
+		middleware.CorsHeaderMiddleware(
+			middleware.JsonHeaderMiddleware(
+				func(w http.ResponseWriter, r *http.Request) {
+					switch r.Method {
+					case "GET":
+						handlers.GetProjects(w, r)
+					case "POST":
+						handlers.PostProject(w, r)
+					}
+				})))
 	http.Handle(prefix+"/projects/",
 		http.StripPrefix(prefix+"/projects/", http.HandlerFunc(
-			func(w http.ResponseWriter, r *http.Request) {
-				// TO DO: Check if the URL.Path is valid
-				switch r.Method {
-				case "GET":
-					handlers.GetProjectByID(w, r)
-				case "DELETE":
-					handlers.DeleteProject(w, r)
-				}
-			})))
+			middleware.CorsHeaderMiddleware(
+				middleware.JsonHeaderMiddleware(
+					func(w http.ResponseWriter, r *http.Request) {
+						// TO DO: Check if the URL.Path is valid
+						switch r.Method {
+						case "GET":
+							handlers.GetProjectByID(w, r)
+						case "DELETE":
+							handlers.DeleteProject(w, r)
+						}
+					})))))
 }
